1f82d21f3df28e0ed11f53b2b05123d1371b57fd
Notjessand <jessennorom@gmail.com>
2024-05-20 10:46:06 -0500

    additional updates

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Hero from "@/components/home/hero";
 
 export default function Page() {
   return (
-    <div className="my-0 py-0 font-custom">
+    <div className="my-0 py-0 font-custom px-10">
       <Header />
       <Hero />
     </div>
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -2,15 +2,30 @@ import React from "react";
 
 const hero = () => {
   return (
-    <div className="px-10 h-[65vh] flex flex-col justify-center">
-      <p className="text-5xl w-1/2 font-bold">
-        Pay with <span className="text-blue-500"> Swapp</span>. Make all your
-        payments in Swapp!
-      </p>
-      <p className="text-2xl text-gray-500 mt-2">
-        {" "}
-        Fast and secure way to purchase or exchange 150+ cryptocurrencies
-      </p>
+    <div className="flex px-10 h-[80vh] pb-14">
+      <div className=" items-center gap-8 w-1/2 flex">
+        <div>
+          <p className="text-5xl w-4/5 font-bold leading-[50px]">
+            Pay with <span className="text-blue-500"> Swapp</span>. Make all
+            your payments in Swapp!
+          </p>
+          <p className="text-2xl text-gray-500 mt-2 w-3/4">
+            {" "}
+            Fast and secure way to purchase or exchange 150+ cryptocurrencies
+          </p>
+          <div className="flex gap-x-5 pt-8 font-montserrat font-bold">
+            <button className="px-6 py-2.5 bg-blue-600 text-white rounded-md hover:bg-blue-500">
+              Get Started
+            </button>
+            <button className="border border-blue-500 px-5 rounded-md hover:bg-slate-700 hover:text-white">
+              Browse Now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className=" w-1/2 flex items-center">
+        <div className="w-96 h-96 bg-purple-400 mx-auto flex items-center"></div>
+      </div>
     </div>
   );
 };
