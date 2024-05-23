5b5ad72ef7edcce70c2800f6d616fa80599d7573
Notjessand <jessennorom@gmail.com>
2024-05-23 04:34:15 -0500

    addition of blog section

--- components/home/blog.tsx
+++ components/home/blog.tsx
@@ -7,12 +7,12 @@ const blog = () => {
       <p className="text-3xl text-center font-semibold font-montserrat text-blue-900">
         Blog
       </p>
-      <div className="grid grid-cols-3 px-28 gap-6 pt-9">
+      <div className="grid md:grid-cols-3 px-5 md:px-16 lg:px-28 gap-6 pt-9 text-center md:text-left">
         <div>
           <img
             src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716453422/2_ryviea.jpg"
             alt=""
-            className="rounded-md"
+            className="rounded-md w-3/4 md:w-full mx-auto"
           />
           <p className="mt-2 text-lg ">Why does Litecoin need MimbleWimble?</p>
           <Link
@@ -26,7 +26,7 @@ const blog = () => {
           <img
             src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716453383/1_ajvpwg.jpg"
             alt=""
-            className="rounded-md"
+            className="rounded-md w-3/4 md:w-full mx-auto"
           />
           <p className="mt-2 text-lg "> How to securely store wallet seeds?</p>
           <Link
@@ -40,7 +40,7 @@ const blog = () => {
           <img
             src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716453781/3_qfnwlb.jpg"
             alt=""
-            className="rounded-md"
+            className="rounded-md w-3/4 md:w-full mx-auto"
           />
           <p className="mt-2 text-lg "> Exclusive Interview With Of Litecoin</p>
           <Link
--- components/home/getInTouch.tsx
+++ components/home/getInTouch.tsx
@@ -1,11 +1,50 @@
 import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+import Link from "next/link";
+import { TfiHeadphoneAlt } from "react-icons/tfi";
+import { MdNewspaper } from "react-icons/md";
+import { IoBriefcaseOutline } from "react-icons/io5";
 
 const getInTouch = () => {
   return (
-    <div className="pt-44 ">
-      <p className="text-2xl font-montserrat font-semibold text-center text-blue-900">
-        Get In Touch
+    <div className="pt-44 min-h-screen">
+      <p className="text-2xl font-montserrat font-semibold text-center text-blue-900 py-12">
+        Get In Touch. Stay in touch.
       </p>
+
+      <div className="grid md:grid-cols-3 px-20 gap-6"></div>
+
+      <div className="grid lg:grid-cols-3 px-14 lg:px-24 gap-x-6 pt-6">
+        <div className="w-full pt-10">
+          <TfiHeadphoneAlt className="h-11 w-11 flex ml-2 text-blue-800" />
+          <p className="text-xl font-semibold font-montserrat pt-7">
+            24/7 support
+          </p>
+          <p className="pt-5 text-lg text-slate-500 font-montserrat">
+            Got a problem? Just get in touch. Our support team is available
+            24/7.
+          </p>
+        </div>
+        <div className="w-full pt-10">
+          <MdNewspaper className="h-10 w-10  ml-2 text-blue-800" />
+          <p className="text-xl font-semibold font-montserrat pt-7">
+            Tendex Blog
+          </p>
+          <p className="pt-5 text-lg text-slate-500 font-montserrat">
+            News and updates from the world&apos;s leading cryptocurrency
+            exchange.
+          </p>
+        </div>
+        <div className="w-full pt-10">
+          <IoBriefcaseOutline className="h-11 w-11 flex ml-2 text-blue-800" />
+          <p className="text-xl font-semibold font-montserrat pt-7">
+            Industry best practices
+          </p>
+          <p className="pt-5 text-lg text-slate-500 font-montserrat">
+            Help build the future of technology. Start your new career at Swapp.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
