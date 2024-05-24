0abc08da052e5cd75eb1ee3b8a14bd2bb14780d7
Notjessand <jessennorom@gmail.com>
2024-05-24 03:12:05 -0500

    addition of custom logo

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -9,7 +9,12 @@ const Header = () => {
   return (
     // <div className=" bg-pink-300">
     <div className="flex py-5 justify-between fixed w-full px-8 md:px-16 bg-white">
-      <div className="w-1/5 h-8 bg-slate-600 flex"></div>
+      <img
+        src="https://res.cloudinary.com/dv62ty87r/image/upload/v1716538239/Premium_Vector___Wolf_tech_modern_simple_logo_icon_ideas-removebg-preview_je6aex.png"
+        alt=""
+        className="w-20"
+      />
+      {/* <div className="w-1/5 h-8 bg-slate-600 flex"></div> */}
 
       <div className="flex lg:hidden items-center justify-center">
         <Sidebar />
